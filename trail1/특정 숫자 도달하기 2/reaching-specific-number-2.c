03bbef020b9b954674b2e134b03d1c9ce5c79489
won956 <won10812@naver.com>
2026-07-16 00:28:17 +0900

    [Passed] 특정 숫자 도달하기 2 (1ms, 0MB)

---
+++ trail1/특정 숫자 도달하기 2/reaching-specific-number-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    scanf("%d", &n);
+
+    for(int i = n; i >= 1; i--) {
+        printf("%d ", i);
+    }
+    return 0;
+}
