bf5808d1f4fc10d1f14cccc6d8c66ba2b4c8b09a
won956 <won10812@naver.com>
2026-07-16 00:01:06 +0900

    [Passed] 정수의 조건 여부 (2ms, 0MB)

---
+++ trail1/정수의 조건 여부/numbers-condition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int a;
+    scanf("%d", &a);
+
+    if(a>=113) {
+        printf("1");
+    } else {
+        printf("0");
+    }
+    return 0;
+}
