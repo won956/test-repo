0750d5b0fba22c568915a10172209c326ad7b2d8
won956 <won10812@naver.com>
2026-07-15 10:47:48 +0900

    [Passed] 0 나올때까지 출력 (1ms, 0MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    while(1) {
+        scanf("%d", &n);
+            if(n == 0) {
+                break;
+            }
+
+            printf("%d\n", n);
+    }
+    return 0;
+}
