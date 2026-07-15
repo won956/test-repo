b13248dae6adcb49f146cc69370a0611509b2c23
won956 <won10812@naver.com>
2026-07-16 00:24:17 +0900

    [Passed] 감소시키며 출력하기 (1ms, 0MB)

---
+++ trail1/감소시키며 출력하기/decrease-and-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int n = 17;
+    scanf("%d", &n);
+
+    for(int i = 17; i>=5; i--) {
+        printf("%d ", i);
+    }
+    return 0;
+}
