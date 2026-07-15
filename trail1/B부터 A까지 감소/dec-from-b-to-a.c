5a78c7a0cacb95bddb3622c3454ca36f97a95975
won956 <won10812@naver.com>
2026-07-16 00:33:16 +0900

    [Passed] B부터 A까지 감소 (1ms, 0MB)

---
+++ trail1/B부터 A까지 감소/dec-from-b-to-a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    for(int i = b; i >= a; i--) {
+        printf("%d ", i);
+    }
+    return 0;
+}
