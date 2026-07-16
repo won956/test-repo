897f91a65858c100c61f66868bd62463c7295393
won956 <won10812@naver.com>
2026-07-16 11:21:04 +0900

    [Passed] 증가시키며 출력하기 2 (2ms, 0MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int a = 10;
+    scanf("%d", &a);
+
+    while(a<=26) {
+        printf("%d ", a);
+        a++;
+    }
+    
+    return 0;
+}
