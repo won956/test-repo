74876041fabaa7afc5865aff547ae5879380548b
won956 <won10812@naver.com>
2026-07-16 11:35:09 +0900

    [Passed] 별 N개 출력하기 (1ms, 0MB)

---
+++ trail1/별 N개 출력하기/print-n-stars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int i = 1;
+    int n;
+    scanf("%d", &n);
+
+    while(i<=n) {
+        printf("*\n");
+        i++;
+    }
+    return 0;
+}
