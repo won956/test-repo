1ec33855ebd68ceaeedfcbeda1b3ac4b64037d2e
won956 <won10812@naver.com>
2026-07-16 12:06:50 +0900

    [Passed] 정사각형 별표 출력하기 (2ms, 0MB)

---
+++ trail1/정사각형 별표 출력하기/print-stars-in-square.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 1; i<=n; i++) {
+        for(int j = 1; j<=n; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
